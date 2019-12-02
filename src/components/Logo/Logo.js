c9504e370c5305ac15f7609b31f81e9663c8c0b2
andresochoa91 <andresochoa91@github.com>
2019-12-01 23:25:21 -0800

    last styles updates

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import racoon from './racoon.png';
 
 const Logo = () => {
 	return(
-		<div className="ma4 mt0">
+		<div className="ma4 mt0 center">
 			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
 				 <div className="Tilt-inner"> <img src={racoon} alt="img"/> </div>
 			</Tilt>			
