04fa82c0ad345fe659b1b6ab062d49d59d1016c2
andresochoa91 <andresochoa91@github.com>
2019-12-01 21:23:33 -0800

    more styles

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import racoon from './racoon.png';
 
 const Logo = () => {
 	return(
 		<div className="ma4 mt0">
 			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-				 <div className="Tilt-inner"> <img src="https://webstockreview.net/images/clipart-brain-angry.png" alt="img"/> </div>
+				 <div className="Tilt-inner"> <img src={racoon} alt="img"/> </div>
 			</Tilt>			
 		</div>
 	);
