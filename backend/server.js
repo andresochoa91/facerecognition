c55f3b0cf8666b782a8efd81e57a658bf8793274
andresochoa91 <andres_ochoa91@hotmail.com>
2020-03-14 22:38:10 -0700

    some changes on profile id backend

--- backend/server.js
+++ backend/server.js
@@ -92,16 +92,20 @@ app.post('/signup', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
+	db.select('*').from('users').where({
+		id: id
+	})
+	.then(user => {
+		if (user.length) {
+			res.json(user[0]);
+		} else {
+			res.status(400).json("Not found");
 		}
 	})
-	if(!found) {
-		res.status(400).json("not found")
-	}
+	.catch(err => res.status(400).json("Error getting user"))
+//	if(!found) {
+//		res.status(400).json("not found")
+//	}
 })
 
 app.put('/image', (req, res) => {
